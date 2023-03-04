c05455c196caad03e067fcf9c1b727dccf826ce0
eng-ivan <eng.ivancarlos85@gmail.com>
2023-03-03 22:20:49 -0300

    fixed ci/cd yml file and created test class

--- src/test/java/core/ics/conta/ContaAgenciaApplicationTests.java
+++ src/test/java/core/ics/conta/ContaAgenciaApplicationTests.java
@@ -2,12 +2,16 @@ package core.ics.conta;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 
 @SpringBootTest
 class ContaAgenciaApplicationTests {
 
+
 	@Test
 	void contextLoads() {
+		System.out.println("Context Load");
 	}
 
 }
